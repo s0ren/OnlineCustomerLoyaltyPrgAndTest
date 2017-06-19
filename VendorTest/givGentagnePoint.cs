765351d572e742dd46a291d839a5d2132e032d05
Søren Magnusson <smag@tec.dk>
2017-06-19 10:06:03 +0200

    Fixed failed test

---
+++ VendorTest/givGentagnePoint.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineCustomerLoyaltyPrgAndTestNS;
+
+namespace VendorTest
+{
+    [TestClass]
+    public class givGentagnePoint
+    {
+
+        ProductVendor Hat_ProdVendor;
+        Customer customer;
+        Customer customer2;
+
+       
+        [TestInitialize]
+        public void Setup()
+        {
+            Hat_ProdVendor = new ProductVendor(1000);
+            customer = new Customer();
+            customer2 = new Customer();
+        }
+
+        [TestMethod]
+        public void requireZoroItems()
+        {
+            int points = Hat_ProdVendor.requirePoints(customer, 0, 0);
+            Assert.AreEqual(0, points);
+        }
+
+        [TestMethod]
+        public void requirePointDestinctItemsTwoDescinctCustomers()
+        {
+            // an item
+            int points = Hat_ProdVendor.requirePoints(customer, 1, 1);
+            Assert.AreEqual(5, points);
+
+            // another item
+            points = Hat_ProdVendor.requirePoints(customer, 2, 1);
+            Assert.AreEqual(11, points);
+
+            // Second Customer
+            // an item
+            points = Hat_ProdVendor.requirePoints(customer2, 1, 1);
+            Assert.AreEqual(5, points);
+
+            // another item
+            points = Hat_ProdVendor.requirePoints(customer2, 2, 1);
+            Assert.AreEqual(11, points);
+        }
+
+        [TestMethod]
+        public void requirePointsTwoItemsDistinctCustomers()
+        {
+            // two of an item
+            int points = Hat_ProdVendor.requirePoints(customer, 1, 2);
+            Assert.AreEqual(10, points);
+
+            // two of another item
+            points = Hat_ProdVendor.requirePoints(customer, 2, 2);
+            Assert.AreEqual(22, points);
+
+            // Second Customer
+            // an item
+            points = Hat_ProdVendor.requirePoints(customer2, 1, 1);
+            Assert.AreEqual(5, points);
+
+            // another item
+            points = Hat_ProdVendor.requirePoints(customer2, 2, 1);
+            Assert.AreEqual(11, points);
+
+            // first item again, two of them
+            points = Hat_ProdVendor.requirePoints(customer2, 1, 2);
+            Assert.AreEqual(10, points);
+
+
+        }
+    }
+}
